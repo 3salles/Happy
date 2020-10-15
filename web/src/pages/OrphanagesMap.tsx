b08c73de7469c92f26c6edeaee911326001d45ea
Beatriz <beatrizsallesss@gmail.com>
2020-10-15 12:11:12 -0300

    ✨ feat: Added map in Map Page.

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
-// import { Map,TileLayer } from 'react-leaflet';
+import { Map, TileLayer } from 'react-leaflet';
 
-// import 'leaflet/dist/leaflet.css'
+import 'leaflet/dist/leaflet.css';
 import mapMarkerImg from '../assets/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
@@ -24,12 +24,12 @@ function OrphanagesMap() {
         </footer>
 
       </aside>
-      <div></div>
-      {/* <Map center={[-2.4480243,-44.1435415]} zoom={15} 
+
+      <Map center={[-2.4480243,-44.1435415]} zoom={15} 
       style={{ width: '100%', height: '100%' }}
       >
         <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
-      </Map> */}
+      </Map>
 
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#fff"/>

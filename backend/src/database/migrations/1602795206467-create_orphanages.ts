699a33a8407dfa0befa06c5ab4259eabd2df7346
Beatriz <beatrizsallesss@gmail.com>
2020-10-15 17:56:09 -0300

    🗃 chore: Created migrations

---
+++ backend/src/database/migrations/1602795206467-create_orphanages.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createOrphanages1602795206467 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}

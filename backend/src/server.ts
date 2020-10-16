96b18abad9555f35debd343abcd7f06dbbdfa9ff
Beatriz <beatrizsallesss@gmail.com>
2020-10-16 00:27:32 -0300

    ✨ feat: Finished API ;P

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import 'express-async-errors';
 
 import './database/connection';
 import routes from './routes';
-import errorHandler from './errors/render';
+import errorHandler from './errors/handler';
 
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
